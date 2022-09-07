0911e4e96b7765936bd57d96cd5a4a24fe90db0d
unknown <leman.miriyeva02@mail.ru>
2022-09-07 09:24:39 +0400

    initial commit

--- task1.js
+++ task1.js
@@ -1,12 +1,23 @@
 var s;
-s=1456541
+s=525
+
+function ToLower(str) {
+  var letterDict = { A: 'a', B: 'b', C: 'c', D: 'd', E: 'e', F: 'f', G: 'g', H: 'h', I: 'i', J: 'j', K: 'k', L: 'l', M: 'm', N: 'n', O: 'o', P: 'p', Q: 'q', R: 'r', S: 's', T: 't', U: 'u', V: 'v', W: 'w', X: 'x', Y: 'y', Z: 'z' }
+
+  var result = ''
+  for (var i = 0; i < str.length; i++)
+    result += letterDict[str[i]] || str[i]
+
+  return result
+}
+
 function checkIsPalindrome(str){
-    str=String(s).toLowerCase()
-    var r = "";
+    str=String(ToLower(str))
+    var str2 = "";
     for(var i = str.length - 1; i >= 0; i--){
-      r += str[i];
+      str2 += str[i];
     }
-    if(str===r){
+    if(str===str2){
         return "is Palindrome"
     }
     
